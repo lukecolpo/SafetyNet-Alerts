dd4d8dbd7c5e1835303e6be65c5855ee14fdc0e5
Luke Colpo <lwcolpo@gmail.com>
2024-10-16 15:18:17 -0400

    finished repository

--- src/main/java/com/example/SafetyNet_Alerts/repository/FireStationRepository.java
+++ src/main/java/com/example/SafetyNet_Alerts/repository/FireStationRepository.java
@@ -1,8 +1,40 @@
 package com.example.SafetyNet_Alerts.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
+
+import com.example.SafetyNet_Alerts.domain.FireStation;
 
 @Repository
 public class FireStationRepository {
 	
+	List<FireStation> fireStations = new ArrayList<FireStation>();
+	
+	public List<FireStation> getList() {
+		return fireStations;
+	}
+	
+	public void addFireStation(FireStation fireStation) {
+		fireStations.add(fireStation);
+	}
+	
+	public FireStation findByAddress(String address) {
+		for (FireStation fireStation : fireStations) {
+			if(fireStation.getAddress().equals(address)) {
+				return fireStation;
+			}
+		}
+		return null;
+	}
+	
+	public FireStation findByStation(String station) {
+		for (FireStation fireStation : fireStations) {
+			if(fireStation.getAddress().equals(station)) {
+				return fireStation;
+			}
+		}
+		return null;
+	}
 }
